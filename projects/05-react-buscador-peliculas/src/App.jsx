1145c5a45d2520f4208623f296c6f0d184c28a9e
csc32 <sanzonettycar@gmail.com>
2024-08-18 17:21:30 -0400

    add new project

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,17 +1,77 @@
 import './App.css';
-import responseMovies from '/mocks/with-result.json';
-import noResultMovies from '/mocks/no-result.json';
+import { useMovies } from './hooks/useMovies';
 import Movies from './components/Movies';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import debounce from 'just-debounce-it';
+import { useCallback } from 'react';
+function useSearch() {
+  const [search, updateSearch] = useState('');
+  const [error, setError] = useState(null);
+  const isFirtsInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirtsInput.current) {
+      isFirtsInput.current = search === '';
+      return;
+    }
+    if (search === '') {
+      setError('No se puede buscar una pelicula vacia');
+      return;
+    }
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar la pelicula con un numero');
+      return;
+    }
+
+    if (search.length < 3) {
+      setError('La busqueda debe ser al menos 3 caracteres');
+      return;
+    }
+    setError(null);
+  }, [search]);
+  return { search, updateSearch, error };
+}
 function App() {
-  const movies = responseMovies.Search;
+  const [sort, setSort] = useState(false);
+  const { search, updateSearch, error } = useSearch();
+  const { movies, getMovies, loading } = useMovies({ search, sort });
+
+  const debouncedGetMovies = useCallback(
+    debounce((search) => {
+      console.log('search', search);
+      getMovies({ search });
+    }, 500),
+    [getMovies],
+  );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getMovies({ search });
+  };
+  const onChange = (e) => {
+    const newSearch = e.target.value;
+    updateSearch(newSearch);
+    debouncedGetMovies(search);
+  };
+  const handleSort = () => {
+    setSort(!sort);
+  };
   return (
     <div className="page">
       <header>
         <h1>Movies Search</h1>
-        <form className="form">
-          <input type="text" placeholder="Avengers, Batman..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={onChange}
+            value={search}
+            placeholder="Avengers, Batman..."
+          />
+          <input type="checkbox" onChange={handleSort} checked={sort} />
           <button type="submit">Search</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error} </p>}
       </header>
       <main
         style={{
@@ -19,7 +79,7 @@ function App() {
           justifyContent: 'center',
         }}
       >
-        <Movies movies={movies}></Movies>
+        {loading ? <p>Loading...</p> : <Movies movies={movies}></Movies>}
       </main>
     </div>
   );
--- projects/05-react-buscador-peliculas/src/components/Movies.jsx
+++ projects/05-react-buscador-peliculas/src/components/Movies.jsx
@@ -1,12 +1,17 @@
 function ListOfMovies({ movies }) {
   return (
-    <ul>
+    <ul className="movies">
       {movies.map((movie) => (
         //use API contract 🙅‍♂️
-        <li key={movie.imdbID}>
+        /*         <li key={movie.imdbID}>
           <h3>{movie.Title}</h3>
           <h4>{movie.Year}</h4>
           <img src={movie.Poster} alt={movie.Title} />
+        </li> */
+        <li key={movie.id} className="movie">
+          <h3>{movie.title}</h3>
+          <h4>{movie.year}</h4>
+          <img src={movie.poster} alt={movie.title} />
         </li>
       ))}
     </ul>
